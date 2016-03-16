ace7db1b76fbcd61c8a76cee1784d3804f1b6ca0
Constantin Dumba <cocodini@hotmail.com>
2016-03-16 14:38:05 -0700

    first commit

---
+++ HappinessMeteor/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace HappinessMeteor.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ HappinessMeteor/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HappinessMeteor.Startup))]
+namespace HappinessMeteor
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
